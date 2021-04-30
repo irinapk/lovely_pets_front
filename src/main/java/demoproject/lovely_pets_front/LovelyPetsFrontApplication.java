0fcb10b6fae82286ed6af8552f94adbdd6fd66ab
Irina Park <park_irina@naver.com>
2021-04-30 22:35:02 +0900

    Initial commit

---
+++ src/main/java/demoproject/lovely_pets_front/LovelyPetsFrontApplication.java
@@ -0,0 +1,13 @@
+package demoproject.lovely_pets_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovelyPetsFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovelyPetsFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demoproject/lovely_pets_front/LovelyPetsFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package demoproject.lovely_pets_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovelyPetsFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
